1260ba805c97821e1dc7339a191d3f1b5a37d7b9
lebhoryi <lebhopryi@gmail.com>
2019-10-07 14:23:12 +0800

    add 2.线性表-双循环链表

--- 2.线性表/2-17.双循环链表.py
+++ 2.线性表/2-17.双循环链表.py
@@ -62,17 +62,16 @@ class DLCLink(object):
             tmp = Node(vals[i])
 
             # add node
-            tmp.next = tmp if i == 0 else move.next
-            # print("tmp.next: ", tmp.next )
-            tmp.prev = tmp if i == 0 else move
-            # print("tmp.prev: ", tmp.prev)
+            if i == 0:  # 第一个节点循环自身
+                tmp.next = tmp
+                tmp.prev = tmp
+            else:
+                tmp.next = move.next
+                tmp.prev = move
+                move.next.prev = tmp
             move.next = tmp
-            # print("move.next: ", move.next)
-            move.next.prev = tmp
-            # print("move.next.prev: ", move.next.prev)
-            # move node
             move = tmp
-        print(tmp.prev)
+
         return move  # 返回尾节点
         # return head.next
 
@@ -100,39 +99,59 @@ class DLCLink(object):
             count += 1
         return count
 
+    @ staticmethod
+    def d_node(link, s, l_link):
+        # del s node
+        if not link or s == 0:
+            return None
+        count, cur = 0, link
+        if s > 0:
+            s = s % l_link if s > l_link else s
+            while count < s:  # 找到第s-1个点
+                cur = cur.next
+                count += 1
+        else:
+            s = abs(s)
+            s = s % l_link if s > l_link else s
+            while count < s:  # 找到倒数第abs(s)个node
+                cur = cur.prev
+                count += 1
+        # 删除第s个node
+        cur.next = cur.next.next
+        cur.next.next.prev = cur
+        return link
+
 
     @ staticmethod
     def res_link(link, s, l_link):
         # 第s+1个节点作为首节点
         if not link or s == 0:
             return None
+        count, cur = 0, link
         if s > 0:
             s = s % l_link if s > l_link else s
-            cur, count = link.next, 1
-            while count < s:  # 找到第s个点
+            while count < s:  # 找到第s-1个点
                 cur = cur.next
                 count += 1
-            return cur
         else:
-            s = -s
+            s = abs(s)
             s = s % l_link if s > l_link else s
-            cur, count = link, 0
-            while count < s:  # 找到第s个点
+            while count < s:  # 找到倒数第abs(s)个node
                 cur = cur.prev
                 count += 1
-            return cur
+        return cur
 
 
 
 if __name__ == "__main__":
-    _list = list(range(1, 6))
-
+    # _list = list(range(1, 6))
+    _list = [chr(i) for i in range(65, 91)]
     # print link
-    print("双向链表： ", end="")
-    link = DLink.c_link(_list)
-    for node in DLink.p_link(link):
-        print(node, end=" ")
-    print()
+    # print("双向链表： ", end="")
+    # link = DLink.c_link(_list)
+    # for node in DLink.p_link(link):
+    #     print(node, end=" ")
+    # print()
 
     print("双向循环链表： ", end="")
     d_link = DLCLink.c_link(_list)
@@ -146,9 +165,16 @@ if __name__ == "__main__":
     # print("The lenth of link: {}.".format(lenth_link))
 
     # 重置链表
-    s = -2
+    s = -3
     d_link = DLCLink.res_link(d_link, s, lenth_link)
     print("重置双向循环链表： ", end="")
+    for node in DLCLink.p_link(d_link):
+        print(node, end=" ")
+    print()
+    print("="*30)
+
+    d_link = DLCLink.d_node(d_link, s, lenth_link)
+    print("删除第{}个节点： ".format(s), end="")
     for node in DLCLink.p_link(d_link):
         print(node, end=" ")
     print()
